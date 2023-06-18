4bc0b597d0f6a282c6eabfce9caf61a58706022f
Vijay Manikanta Reddy Sathi <83952736+vijaymanikantareddy@users.noreply.github.com>
2023-06-18 17:27:21 +0530

    Update fetch_submissions.py

--- fetch_submissions.py
+++ fetch_submissions.py
@@ -3,32 +3,29 @@ import subprocess
 # Install matplotlib using pip
 subprocess.check_call(['pip', 'install', 'matplotlib'])
 
-import os
-import json
 import requests
+import json
 import matplotlib.pyplot as plt
-from matplotlib.dates import DateFormatter
 
-# Retrieve LeetCode submissions
-response = requests.get('https://leetcode.com/api/user_submission_calendar/vijaymanikantareddy/')
-data = response.json()
+# Make the API request
+response = requests.get('https://api.example.com/leetcode/submissions')
 
-# Extract submission data
-submissions = data['submissions']
-dates = [submission['date'] for submission in submissions]
-counts = [submission['count'] for submission in submissions]
+try:
+    # Check if the response is successful
+    response.raise_for_status()
 
-# Convert dates to matplotlib format
-formatted_dates = [date[:4] + '-' + date[4:6] + '-' + date[6:] for date in dates]
-formatted_dates = [plt.datetime.strptime(date, "%Y-%m-%d").date() for date in formatted_dates]
+    # Parse the JSON data
+    data = response.json()
 
-# Generate the graph
-fig, ax = plt.subplots()
-ax.plot(formatted_dates, counts, color='blue')
-ax.xaxis.set_major_formatter(DateFormatter("%b '%y"))
-ax.set_xlabel('Date')
-ax.set_ylabel('Submissions')
-ax.set_title('LeetCode Submissions')
+    # Process the data and generate the graph
+    # ...
+
+except requests.exceptions.HTTPError as e:
+    print(f"HTTP Error: {e}")
+except json.decoder.JSONDecodeError as e:
+    print(f"JSON Decode Error: {e}")
+except Exception as e:
+    print(f"An error occurred: {e}")
 
 # Save the graph as SVG
 plt.savefig('leetcode_graph.svg')
